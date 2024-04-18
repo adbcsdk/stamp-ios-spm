95f3676abf9b9b09fddeab95154190021734eff1
metroidsub <metroidsub@gmail.com>
2024-04-18 15:06:45 +0900

    Initial Commit

---
+++ Framework/StampSDKBF.xcframework/ios-arm64/StampSDKBF.framework/Headers/StampSDKBF.h
@@ -0,0 +1,18 @@
+//
+//  StampSDKBF.h
+//  StampSDKBF
+//
+//  Created by 신아람 on 4/15/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StampSDKBF.
+FOUNDATION_EXPORT double StampSDKBFVersionNumber;
+
+//! Project version string for StampSDKBF.
+FOUNDATION_EXPORT const unsigned char StampSDKBFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StampSDKBF/PublicHeader.h>
+
+
